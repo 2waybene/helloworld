5bbbab03e328acc6f5145f5e33ca814c607aab95
Jianying Li <li11@MacBook-Pro.local>
2017-10-18 10:14:02 -0400

    changes for R topic

--- IB_group_meeting/rforest/execute_rmd.R
+++ IB_group_meeting/rforest/execute_rmd.R
@@ -4,10 +4,10 @@ library(knitr)
 
 
 ##  column7: breastfeeding status
-render("x:/myGit/helloworld/IB_group_meeting/RmdProj1/proj1_in_pdf.rmd", 
+render("/Users/li11/myGit/helloworld/IB_group_meeting/RmdProj1/proj1_in_pdf.rmd", 
        params = list( brfStatus = 7, 
                       sesStatus = 4, 
                       genStatus = 2, 
                       regStatus = 3), 
-       output_file = "x:/myGit/helloworld/IB_group_meeting/analysisResults/RSV_GoldenGate.pdf")
+       output_file = "/Users/li11/myGit/helloworld/IB_group_meeting/RSV_GoldenGate.pdf")
 
